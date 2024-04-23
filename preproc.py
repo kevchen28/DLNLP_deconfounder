157a8718812f509c7ed903b6f4028995c911d770
Bongwoo Jeon <qhddn2643@gmail.com>
2024-04-23 16:53:38 -0400

    Modified preproc.py and generated BlogCatalog1 datasets and visualizations

--- preproc.py
+++ preproc.py
@@ -1,16 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[64]:
 import scipy.io as sio
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib import rc, rcParams
+
 #visualize everything using tsne
-# from tsne import tsne
-# from tsne import pylab
 from sklearn.manifold import TSNE
 
 from sklearn.decomposition import LatentDirichletAllocation
@@ -24,14 +19,10 @@ path = './datasets/'
 name = 'BlogCatalog'
 data = sio.loadmat(path+name+'/data.mat')
 
-
-# In[76]:
-
-
 #setting constants
 kappa1 = 10
-kappa2 = 10
-C = 1
+kappa2 = 1
+C = 5
 att = "something"
 
 if kappa2 != 0.1:
@@ -39,25 +30,11 @@ if kappa2 != 0.1:
 else:
 	extra_str = ''
 
-if att!="":
-	extra_str += 's'
-
-
-# In[3]:
-
-
 data.keys()
-
-# In[126]:
-
 
 X = data['Attributes']
 n=X.shape[0]
 X.tocoo()
-
-
-# In[13]:
-
 
 A = data['Network']
 A_dense = np.array(A.todense())
@@ -75,58 +52,22 @@ if att!="":
 
 Reweighted_Sparse_A = sparse.csr_matrix(A_dense)
 
-# In[6]:
-
-
-#check if A is symmetric
-# def check_symmetric(a, tol=1e-8):
-#     return np.allclose(a, a.T, atol=tol)
-# check_symmetric(A_dense)
-
-
-# In[7]:
-
-
 #get 50 topics
 lda = LatentDirichletAllocation(n_components=50)
 lda.fit(X)
 
-
-# In[8]:
-
-
 Z = lda.transform(X)
 
-
-# In[15]:
-
-
 AZ = np.matmul(A_dense,Z)
-
-
-# In[18]:
-
-
-# AZ.shape
-
-
-# In[25]:
-
+Z = lda.transform(X)
+AZ = np.matmul(A_dense,Z)
 
 #random sample an instance and use its topic distribution as the centroid
 for exp_id in range(0,10):
 	centroid1_idx = random.randint(0, X.shape[0]-1)
 	Z_c1 = Z[centroid1_idx,:]
-
-
-	# In[26]:
-
-
 	Z_c0 = np.mean(Z,axis=0)
-
-
-	# In[35]:
-
+	Z_c0 = np.mean(Z,axis=0)
 
 	#precompute the similarity between each instance and the two centroids
 	ZZ_c1 = np.matmul(Z,Z_c1)
@@ -134,78 +75,43 @@ for exp_id in range(0,10):
 	AZZ_c1 = np.matmul(AZ,Z_c1)
 	AZZ_c0 = np.matmul(AZ,Z_c0)
 
-
-	# In[77]:
-
-
 	#get propensity for each instance
 	p1 = kappa1*ZZ_c1+kappa2*AZZ_c1
 	p0 = kappa1*ZZ_c0+kappa2*AZZ_c0
 	propensity = np.divide(np.exp(p1), np.exp(p1)+np.exp(p0))
-
-
-	# In[78]:
-
-
 	ps = pd.Series(np.squeeze(propensity))
 	ps.describe()
 
-
-	# In[113]:
-
+	#visualize the propensity distribution
+	ps = pd.Series(np.squeeze(propensity))
+	ps.describe()
 
 	#visualize the propensity distribution
-	# %matplotlib inline
 	fig0, ax0 = plt.subplots()
 	ax0.hist(propensity,bins=50)
 	plt.title('propensity score distribution')
 	plt.xlabel('propensity score')
 	plt.ylabel('frequency')
 	plt.savefig('./figs/'+name+extra_str+str(exp_id)+'ps_dist.pdf',bbox_inches='tight')
-	# plt.show()
-
-
-	# In[80]:
-
 
 	#simulate treatments
 	T = np.random.binomial(1, p=propensity)
 
-
-	# In[81]:
-
-
-	# plt.hist(T,bins=50)
-	# plt.title('Treatment')
-	# plt.savefig('./figs/'+name+'ps_dist.pdf',bbox_inches='tight')
-	# plt.show()
-
-
-	# In[82]:
-
+	#sample noise from Gaussian
+	epsilon = np.random.normal(0,1,X.shape[0])
 
 	#sample noise from Gaussian
 	epsilon = np.random.normal(0,1,X.shape[0])
 
-	# In[83]:
-
 	#simulate outcomes
 	Y1 = C*(p1+p0)+epsilon
 	Y0 = C*(p0)+epsilon
-
-	# In[112]:
-
 	fig1, ax1 = plt.subplots()
 	ax1.hist(Y1,bins=50,label='Treated')
 	ax1.hist(Y0,bins=50,label='Control')
 	plt.title('outcome distribution')
 	plt.legend()
 	plt.savefig('./figs/'+name+extra_str+str(exp_id)+'outcome_dist.pdf',bbox_inches='tight')
-	# ax1.show()
-
-
-	# In[111]:
-
 
 	#distribution of ITE
 	fig2, ax2 = plt.subplots()
@@ -216,32 +122,12 @@ for exp_id in range(0,10):
 	ax2.axvline(x=np.mean(Y1-Y0),color='red',label='ATE')
 	plt.savefig('./figs/'+name+extra_str+str(exp_id)+'ite_dist.pdf',bbox_inches='tight')
 	ax2.legend()
-	# plt.show()
-
-
-	# In[86]:
-
 
 	print('ATE is %.3f'%(np.mean(Y1-Y0)))
 
-
-	# In[87]:
-
-
 	#save the data
 	#save Y1 Y0 T X
-
-
-	# In[88]:
-
-
-	# Z_ = tsne(Z, 2, 50, 20.0)
 	Z_ = TSNE(n_components=2, perplexity=50, early_exaggeration=20.0).fit_transform(Z)
-
-
-	# In[110]:
-
-
 	labels = T #use treatment as the binary label
 	treated_idx = np.where(T==1)[0]
 	controled_idx = np.where(T==0)[0]
@@ -258,45 +144,18 @@ for exp_id in range(0,10):
 	plt.savefig('./figs/'+name+extra_str+'tsne.pdf',bbox_inches='tight')
 	plt.legend(loc=2)
 	plt.xlim(-100,100)
-	#pylab.show()
-
-
-	# In[114]:
-
 
 	#get the most freq 100 words of each topic
 	topics = lda.components_
 
-
-	# In[115]:
-
-
 	#calculate the topic 100 words in each topic
 	topics_100_dims = np.argsort(topics,axis=1)[:,-100:]
-
-
-	# In[116]:
-
 
 	#then we get a union of all those top 100 words
 	unique_100_dims = np.unique(topics_100_dims)
 
-
-	# In[117]:
-
-
 	#reduce the dimensions by extract the selected words
 	X_100 = X[:,unique_100_dims]
-
-
-	# In[118]:
-
-
-	# X_100.shape
-
-
-	# In[122]:
-
 
 	#save the data
 	sio.savemat('./datasets/'+name+extra_str+'/'+name+str(exp_id)+'.mat',{
