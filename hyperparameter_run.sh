75d73c3814c3571f33dc04b118c9e6de8a18236c
prototypeanugrah <prototype.anugrah@gmail.com>
2024-04-24 23:04:33 +0000

    Run for BlogCatalog - 0.5, 1, 2

--- hyperparameter_run.sh
+++ hyperparameter_run.sh
@@ -2,7 +2,7 @@ for i in 1 2
 do
 	for j in 1 2 3
 	do
-    	python main.py --tr 0.6 --path ./datasets/ --dropout 0.1 --weight_decay 1e-4 --alpha 1e-4 --lr 1e-2 --epochs 200 --dataset BlogCatalog1 \
+    	python main.py --tr 0.6 --path ./datasets/ --dropout 0.1 --weight_decay 1e-4 --alpha 1e-4 --lr 1e-2 --epochs 200 --dataset BlogCatalog2 \
 	 --norm 1 --nin $i --nout $j --hidden 100 --clip 100.
 	done
 done
